7182e42efff4eaac34337d66d463ff232c3eae08
jeremy.d-auria <jeremy.d-auria@epita.fr>
2021-06-19 17:51:26 +0200

    fix: compile gpu neighbors

--- src/neighbors/knn.cu
+++ src/neighbors/knn.cu
@@ -1,26 +1,29 @@
+#include <iostream>
+#include "knn.cuh"
+
 #define N_CLUSTERS = 16
 #define TILE_SIZE = 256 // A MOVE DANS UN HH
 
 
-__global__ compute_nearest_neighbors(const int* histo_tab,
+__global__ void compute_nearest_neighbors(const int* histo_tab,
                                         const size_t histo_pitch,
                                         const float* clusters,
                                         const size_t cluster_pitch,
-                                        int results,
+                                        int* results,
                                         const size_t tiles_number)
 {
     size_t x = threadIdx.x;
     size_t y = threadIdx.y;
     size_t tile_index = blockIdx.x;
 
-    if (x >= TILE_SIZE || y >= N_CLUSTERS || tile_index >= tiles_number)
+    if (x >= 256 || y >= 16 || tile_index >= tiles_number)
         return;
 
-     __shared__ int cluster_distances[N_CLUSTERS];
+     __shared__ int cluster_distances[16];
 
     cluster_distances[y] = 0;
 
-    __synchthreads();
+    __syncthreads();
 
 
     int value = *(histo_tab + tile_index * histo_pitch + x);
@@ -36,7 +39,7 @@ __global__ compute_nearest_neighbors(const int* histo_tab,
 
     auto result_ptr = results + tile_index;
     *result_ptr = 0;
-    for (int i = 1; i < N_CLUSTERS; i++)
+    for (int i = 1; i < 16; i++)
     {
         if (cluster_distances[*result_ptr] > cluster_distances[i])
             *result_ptr = i;
@@ -49,7 +52,7 @@ void k_nearest_neighbors(const int* histo_tab, const float* clusters, const size
 
     int* cuda_histo_tab;
     size_t cuda_histo_tab_pitch;
-    auto tile_byte_size = TILE_SIZE * sizeof(int);
+    auto tile_byte_size = 256 * sizeof(int);
 
     rc = cudaMallocPitch(&cuda_histo_tab, &cuda_histo_tab_pitch, tile_byte_size, tiles_number);
     if (rc)
@@ -71,9 +74,9 @@ void k_nearest_neighbors(const int* histo_tab, const float* clusters, const size
 
     float* cuda_clusters;
     size_t cuda_clusters_pitch;
-    auto cluster_byte_size = TILE_SIZE * sizeof(float);
+    auto cluster_byte_size = 256 * sizeof(float);
 
-    rc = cudaMallocPitch(&cuda_clusters, &cuda_clusters_pitch, cluster_byte_size, N_CLUSTERS);
+    rc = cudaMallocPitch(&cuda_clusters, &cuda_clusters_pitch, cluster_byte_size, 16);
     if (rc)
     {
         std::cout << "Could not allocate memory for clusters"
@@ -82,7 +85,7 @@ void k_nearest_neighbors(const int* histo_tab, const float* clusters, const size
     }
 
     rc = cudaMemcpy2D(cuda_clusters, cuda_clusters_pitch, clusters, cluster_byte_size,
-                        cluster_byte_size, N_CLUSTERS, cudaMemcpyHostToDevice);
+                        cluster_byte_size, 16, cudaMemcpyHostToDevice);
     if (rc)
     {
         std::cout << "Could not copy memory for clusters"
@@ -100,11 +103,11 @@ void k_nearest_neighbors(const int* histo_tab, const float* clusters, const size
         exit(EXIT_FAILURE);
     }
 
-    dim3 block_dim(TILE_SIZE, N_CLUSTERS);
+    dim3 block_dim(256, 16);
     compute_nearest_neighbors<<<block_dim, tiles_number>>>(cuda_histo_tab, cuda_histo_tab_pitch,
         cuda_clusters, cuda_clusters_pitch, result, tiles_number);
 
 
-    int* output = malloc(sizeof(int) * tiles_number);
-    rc = cudaMemcpy(output, result, sizeof(int) tiles_number, cudaMemcpyDeviceToHost);
-
+    int* output = (int*) malloc(sizeof(int) * tiles_number);
+    rc = cudaMemcpy(output, result, sizeof(int) * tiles_number, cudaMemcpyDeviceToHost);
+}
---
+++ src/neighbors/knn.cuh
@@ -0,0 +1,6 @@
+#pragma once
+
+
+void k_nearest_neighbors(const int* histo_tab, const float* clusters, const size_t tiles_number);
+
+
