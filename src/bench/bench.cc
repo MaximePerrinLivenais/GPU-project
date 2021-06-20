877fc80a87f0f274afb7ab05bbd4cd9ee2f2feb7
jeremy.d-auria <jeremy.d-auria@epita.fr>
2021-06-21 00:08:13 +0200

    feat: add basic benchmark

---
+++ src/bench/bench.cc
@@ -0,0 +1,12 @@
+#include <benchmark/benchmark.h>
+#include "misc/image-load.hh"
+#include "lbp/lbp.cuh"
+
+static void BM_lbp(benchmark::State& state)
+{
+    auto image = load_image("../data/images/barcode-00-01.jpg");
+    for (auto _ : state)
+        compute_lbp_values(image.data, image.cols, image.rows);
+}
+BENCHMARK(BM_lbp)->Unit(benchmark::kMillisecond)->UseRealTime();
+BENCHMARK_MAIN();
