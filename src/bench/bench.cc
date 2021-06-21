c336e5690d3b60e5a9fdf421672fdfdc7a15b2fe
Youssef Ouhmmou <youssef.ouhmmou@epita.fr>
2021-06-21 16:47:07 +0200

    bench: loop in args
    Signed-off-by: Youssef Ouhmmou <youssef.ouhmmou@epita.fr>

--- src/bench/bench.cc
+++ src/bench/bench.cc
@@ -1,11 +1,17 @@
-#include <benchmark/benchmark.h>
 #include "misc/image-load.hh"
 #include "lbp/lbp.cuh"
 #include "cpu/lbp.hh"
+#include <opencv2/imgproc.hpp>
+#include <benchmark/benchmark.h>
 
 static void BM_lbp_cpu(benchmark::State& state)
 {
     auto image = load_image("../data/images/barcode-00-01.jpg");
+
+    auto rows = state.range(0);
+
+    cv::resize(image, image, cv::Size(rows, rows));
+
     for (auto _ : state)
         compute_lbp_values_cpu(image, 16);
 }
@@ -13,10 +19,26 @@ static void BM_lbp_cpu(benchmark::State& state)
 static void BM_lbp_gpu(benchmark::State& state)
 {
     auto image = load_image("../data/images/barcode-00-01.jpg");
+
+    auto rows = state.range(0);
+
+    cv::resize(image, image, cv::Size(rows, rows));
+
     for (auto _ : state)
         compute_lbp_values(image.data, image.cols, image.rows);
 }
 
-BENCHMARK(BM_lbp_cpu)->Unit(benchmark::kMillisecond)->UseRealTime();
-BENCHMARK(BM_lbp_gpu)->Unit(benchmark::kMillisecond)->UseRealTime();
+static void custom_arguments(benchmark::internal::Benchmark* b)
+{
+    for (auto i = 16; i <= 16384; i *= 2)
+        b->Args({i});
+}
+
+BENCHMARK(BM_lbp_cpu)
+    ->Apply(custom_arguments)
+    ->Unit(benchmark::kMillisecond)->UseRealTime();
+
+BENCHMARK(BM_lbp_gpu)
+    ->Apply(custom_arguments)
+    ->Unit(benchmark::kMillisecond)->UseRealTime();
 BENCHMARK_MAIN();
